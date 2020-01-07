2d1ad1308d2ce717deaefded061cad93e9fece1c
unknown <thesorakhan@gmail.com>
2020-01-07 15:15:06 +1100

    added Add Client form with validation

--- src/app/components/add-client-form/add-client-form.component.ts
+++ src/app/components/add-client-form/add-client-form.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Client } from '../../models/Client';
+
 @Component({
   selector: 'app-add-client-form',
   templateUrl: './add-client-form.component.html',
   styleUrls: ['./add-client-form.component.css']
 })
 export class AddClientFormComponent implements OnInit {
+
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  }
+
+  disableBalanceOnAdd: boolean = true;
+
 
   constructor() { }
 
--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -10,11 +10,27 @@ import { Client } from '../../models/Client'
 })
 export class ClientsComponent implements OnInit {
   clients : Client[];
+  totalOwed : number;
 
   constructor(private clientService : ClientService) { }
 
   ngOnInit() {
-    this.clientService.getClients().subscribe(clients => this.clients = clients);
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    });
+    
+  }
+
+  // could use a for loop and append the total value
+  // but we use reduce() which is a higher order array method and modern to use !!
+  // reduce() works similar to forEach
+  getTotalOwed() {
+    // reduce() takes two params:
+    // previous value to add new value to and value of the current client we iterationg thru
+    this.totalOwed = this.clients.reduce((total, currentClient) => {
+      return total + currentClient.balance;
+    }, 0); // the 0 is the initial value of total
   }
 
 }
